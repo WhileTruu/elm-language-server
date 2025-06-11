f852984f3a2419430bea109f88c41de6897c3829
Adam DiCarlo <adam@adaptiv.systems>
2025-06-10 17:33:10 -0700

    Enable building with Nix flake

---
+++ nix/packages/default.nix
@@ -0,0 +1,34 @@
+{
+  pkgs,
+  lib,
+  makeWrapper,
+}: self:
+pkgs.haskell.packages.ghc96.override {
+  overrides = self: super: let
+    inherit (pkgs.haskell.lib.compose) overrideCabal;
+    elmPkgs = rec {
+      elm-language-server = overrideCabal (drv: {
+        # sadly parallelism most of the time breaks compilation
+        enableParallelBuilding = false;
+        patches = [];
+        buildTools = drv.buildTools or [] ++ [makeWrapper];
+
+        description = "Fork of the elm compiler, repurposed for a language server.";
+        homepage = "https://github.com/WhileTruu/elm-language-server";
+        license = lib.licenses.bsd3;
+        maintainers = with lib.maintainers; [];
+        mainProgram = "elm-language-server";
+      }) (self.callPackage ./elm-language-server {});
+
+      elmVersion = elmPkgs.elm-language-server.version;
+    };
+  in
+    elmPkgs
+    // {
+      inherit elmPkgs;
+
+      ansi-wl-pprint = overrideCabal (drv: {
+        jailbreak = true;
+      }) (self.callPackage ./ansi-wl-pprint {});
+    };
+}
